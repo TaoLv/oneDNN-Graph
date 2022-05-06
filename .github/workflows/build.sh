da64bc33ac51a085fd7eb80999d5283df93adab3
Lv, Tao A <tao.a.lv@intel.com>
2022-05-06 13:21:11 +0800

    github: workflow: add build script

---
+++ .github/workflows/build.sh
@@ -0,0 +1,83 @@
+#! /bin/bash
+
+set -x
+
+ls -a
+
+git clone https://github.com/oneapi-src/oneDNN.git --branch dev-graph --recursive
+cd oneDNN
+mkdir build
+cd build
+
+# build latest
+cmake ..
+make doc
+cd ../..
+
+rm -rf ./latest
+mkdir latest
+cp -r ./oneDNN/build/reference/html/* ./latest/
+
+# build alpha
+cd oneDNN
+git checkout origin/dev-graph-alpha
+git submodule update --recursive
+cd build
+rm -rf ./*
+cmake ..
+make doc
+cd ../..
+
+rm -rf ./alpha
+mkdir alpha
+cp -r ./oneDNN/build/reference/html/* ./alpha/
+
+# build preview4
+cd oneDNN
+git checkout origin/dev-graph-preview4
+git submodule update --recursive
+cd build
+rm -rf ./*
+cmake ..
+make doc
+cd ../..
+
+rm -rf ./preview4
+mkdir preview4
+cp -r ./oneDNN/build/reference/html/* ./preview4/
+
+# build preview3
+cd oneDNN
+git checkout origin/dev-graph-preview3
+git submodule update --recursive
+cd build
+rm -rf ./*
+cmake ..
+make doc
+cd ../..
+
+rm -rf ./preview3
+mkdir preview3
+cp -r ./oneDNN/build/reference/html/* ./preview3/
+
+# build preview2
+cd oneDNN
+git checkout origin/dev-graph-preview2
+git submodule update --recursive
+cd build
+rm -rf ./*
+cmake ..
+make doc
+cd ../..
+
+rm -rf ./preview2
+mkdir preview2
+cp -r ./oneDNN/build/reference/html/* ./preview2/
+
+echo "finished document build"
+
+ls -a
+
+
+
+
